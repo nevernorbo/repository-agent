dac170ed95b27a4ea9b2630eba7cafb647390cb5
Norbert Sári <sarinorbert555@gmail.com>
2026-02-23 23:44:24 +0100

    showcase time

--- agent/config.py
+++ agent/config.py
@@ -1,4 +1,6 @@
 from google.adk.models.lite_llm import LiteLlm
 
-# MODEL = LiteLlm(model="ollama_chat/qwen3:4b")
-MODEL = "gemini-2.5-flash-lite"
+# MODEL = LiteLlm(
+#     model="ollama_chat/qwen3:4b", api_base="http://host.docker.internal:11434"
+# )
+MODEL = "gemma-3-27b"
